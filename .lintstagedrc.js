f2ada875bd61368e5e4b97c4d112a612e99b2f6c
lagu <lagu.dilemma1729@gmail.com>
2024-10-03 21:52:45 +0900

    initial commit

---
+++ .lintstagedrc.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`;
+
+module.exports = {
+  '*.{js,cjs,mjs,json,ts,tsx,css}': ['prettier --write'],
+  '*.{ts,tsx}': ['bash -c tsc --noEmit', buildEslintCommand, 'markuplint'],
+};
