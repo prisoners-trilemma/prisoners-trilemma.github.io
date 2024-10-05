e3b968a7447be1ba2fae39e937c9e100a6bbe003
lagu <lagu.dilemma1729@gmail.com>
2024-10-05 17:49:32 +0900

    deleted fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,5 @@
-import localFont from 'next/font/local';
-
 import type { Metadata } from 'next';
 import './globals.css';
-
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-});
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
-});
 
 export const metadata: Metadata = {
   title: "Prisoners' Trilemma",
@@ -26,7 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body className={`antialiased`}>{children}</body>
     </html>
   );
 }
