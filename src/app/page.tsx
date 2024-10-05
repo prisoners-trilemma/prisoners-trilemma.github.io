73a8fec350d7cad75cb3f8c96c1127410307703c
lagu <lagu.dilemma1729@gmail.com>
2024-10-05 22:06:34 +0900

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,19 @@ import React from 'react';
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 
+import { jacquarda, newTegomin } from '@/utils/font';
+
 export default function Home() {
   return (
     <>
       <Header />
-      <main>
+      <main className={`${newTegomin.className}`}>
+        <section>クリエイターだったり日曜学者だったりゲーマーだったりする人たちです。</section>
         <section>
-          <h1>What's new</h1>
+          <h1 className={`${jacquarda.className} text-4xl font-bold`}>Articles</h1>
         </section>
         <section>
-          <h1>Products</h1>
+          <h1 className={`${jacquarda.className} text-4xl font-bold`}>Products</h1>
         </section>
       </main>
       <Footer />
